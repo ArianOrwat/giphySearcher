dae50d6c555c763862388eea03c5e144507a2876
Arian Orwat <arian.orwat@gmail.com>
2019-11-03 00:27:39 +0100

    Add Result component

--- src/App.vue
+++ src/App.vue
@@ -1,20 +1,21 @@
 <template>
   <div id="app">
     <Search msg="Hello World" v-on:result="result = $event" v-on:error="error = $event"/>
-    <div v-for="item in result" :key="item.title" class="image">
-      <img :src="item.images.downsized.url" :alt="item.title">
+    <div class="grid">
+      <Result v-for="item in result" :key="item.title + ' ' + item.import_datetime" :src="item.images.fixed_width.url" :alt="item.title"/>
     </div>
     <h4 id="error">{{error}}</h4>
   </div>
 </template>
 <script>
 import Search from './components/Search.vue';
-// import Result from './components/Result.vue';
+import Result from './components/Result.vue';
 
 export default {
   name: 'app',
   components: {
-    Search
+    Search,
+    Result
   },
   data() {
     return {
@@ -25,6 +26,21 @@ export default {
 }
 </script>
 
-<style>
+<style lang="scss">
+  .grid {
+    display: grid;
+    grid-template-columns: auto auto auto;
+  }
 
+  @media only screen and (max-width: 1200px) {
+    .grid {
+      grid-template-columns: auto auto;
+    }
+  }
+
+  @media only screen and (max-width: 600px) {
+    .grid {
+      grid-template-columns: auto;
+    }
+  }
 </style>
--- src/components/Result.vue
+++ src/components/Result.vue
@@ -1,13 +1,26 @@
 <template>
-    
+    <div class="image">
+        <img :src="src" :alt="alt">
+    </div>
 </template>
 
 <script>
 export default {
     name: 'Result',
+    props: {
+        src: String,
+        alt: String,
+    }
 }
 </script>
 
-<style lang="sass">
-
+<style lang="scss">
+    .image {
+        width: 100%;
+        display: flex;
+        img {
+            justify-content: center;
+            align-items: center;
+        }
+    }
 </style>
